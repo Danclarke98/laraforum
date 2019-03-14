3d8dba258cfb1861c0dee1fddce6382f12b819e7
popdev <danclarke98@gmail.com>
2019-03-14 03:33:59 +0000

    Auth Implemented

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Forum</title>
+
+        <link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    </head>
+
+
+    <body>
+
+       <div class="navbar">
+            <a class="navbar-brand" href="#">Laravel Forum</a>
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="#">Home</a>
+
+
+                </li>
+
+
+
+            </ul>
+
+
+
+       </div>
+
+
+
+
+
+
+
+
+    </body>
+
+
+
+
+
+</html>
